1acc3393a03afe5107b9ac103de984c07143c85b
bhbs <bhbstar.me@gmail.com>
2022-09-25 21:36:20 +0900

    chore: set docs base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Viject',
   description: 'Ejecting and Migrating tool for Vite compatible with Create React App (react-scripts)',
-  base: "",
+  base: "/viject/",
 })
