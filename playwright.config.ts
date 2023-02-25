bc3a932158f8629fcfc407723f2a76e68d144f98
bhbs <bhbstar.me@gmail.com>
2023-02-25 11:23:11 +0900

    chore: install playwright

---
+++ playwright.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+	testDir: "./e2e",
+	timeout: 30 * 1000,
+	expect: {
+		timeout: 5000,
+	},
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
+	use: {
+		actionTimeout: 0,
+		baseURL: "http://localhost:3000/PUBLIC_URL/",
+		trace: "on-first-retry",
+	},
+
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+
+		{
+			name: "webkit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
+
+	// webServer: {
+	//   command: 'npm run start',
+	//   port: 3000,
+	// },
+});
