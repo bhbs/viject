7d6478c09ce71082e9bce593d6fd3d0ef75e5185
bhbs <bhbstar.me@gmail.com>
2023-04-17 09:52:45 +0900

    docs: configure local search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,4 +4,9 @@ export default defineConfig({
   title: 'Viject',
   description: 'Ejecting and Migrating tool for Vite compatible with Create React App (react-scripts)',
   base: "/viject/",
+  themeConfig: {
+    search: {
+      provider: 'local'
+    }
+  }
 })
