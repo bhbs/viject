16c3e1a9c6f8ba33047c64a037283ec38a1cd20e
Taisei Mima <bhbstar.me@gmail.com>
2024-06-02 12:09:02 +0900

    test: e2e

--- projects/kitchensink/src/features/webpack/CssModulesInclusion.tsx
+++ projects/kitchensink/src/features/webpack/CssModulesInclusion.tsx
@@ -5,7 +5,7 @@ import indexStyles from "./assets/index.module.css";
 export const CssModulesInclusion = () => (
 	<div id="feature-css-modules-inclusion">
 		<p className={styles.cssModulesInclusion}>CSS Modules are working!</p>
-		<p className={indexStyles.cssModulesInclusion}>
+		<p className={indexStyles.cssModulesIndexInclusion}>
 			CSS Modules with index are working!
 		</p>
 	</div>
